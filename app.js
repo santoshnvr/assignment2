906675375af85ae356c13d0c781303116b84e417
santosh <santoshnvr@gmail.com>
2022-02-21 16:54:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.get('/', (req, res) => {
     res.send("Hello world!!!");
 });
 
-app.get('/getFirstThousandRecords', (req, res) => {
-    let query = "SELECT TOP (10) * FROM all_month";
+app.get('/getTenthQuery', (req, res) => {
+    let query = "Select time, place, latitude, longitude  from eq where mag > 3.14 and mag < 3.24";
     const request = new Request(query, (err, rowCount) => {
         if(err) {
             console.log(err.message);
@@ -31,8 +31,64 @@ app.get('/getFirstThousandRecords', (req, res) => {
     connection.execSql(request);
 });
 
+app.get('/getEleventhQuery', (req, res) => {
+    let query = "Select time, place, latitude, longitude  from eq where mag > 3.14 and mag < 3.24";
+    const request = new Request(query, (err, rowCount) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            console.log(`Number of rows returned ${rowCount}`);
+        }
+    });
+    let rows = [];
+    request.on("row",(columns) => {
+        rows.push(columns);
+    })
+    request.on('doneInProc', (rowCount, more) => {
+        res.send(rows);
+    });
+    connection.execSql(request);
+});
 
-const port = process.env.PORT || 3000;
+app.get('/getTwelthQuery', (req, res) => {
+    let query = "Select time, place, latitude, longitude  from eq where mag > 3.14 and mag < 3.24Select time, place, latitude, longitude  from eq where mag > 3.14 and mag < 3.24 and place LIKE '%CA%'";
+    const request = new Request(query, (err, rowCount) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            console.log(`Number of rows returned ${rowCount}`);
+        }
+    });
+    let rows = [];
+    request.on("row",(columns) => {
+        rows.push(columns);
+    })
+    request.on('doneInProc', (rowCount, more) => {
+        res.send(rows);
+    });
+    connection.execSql(request);
+});
+
+app.get('/getThirteenthQuery', (req, res) => {
+    let query = "Select time, place, latitude, longitude  from eq where mag > 3.14 and mag < 3.24";
+    const request = new Request(query, (err, rowCount) => {
+        if(err) {
+            console.log(err.message);
+        } else {
+            console.log(`Number of rows returned ${rowCount}`);
+        }
+    });
+    let rows = [];
+    request.on("row",(columns) => {
+        rows.push(columns);
+    })
+    request.on('doneInProc', (rowCount, more) => {
+        res.send(rows);
+    });
+    connection.execSql(request);
+});
+
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
